def7d16aa50536be2e507c480cb222bfae90255f
Adan-Sa <adan.saada11@gmail.com>
2021-08-18 16:34:33 +0300

    update

--- src/pages/Request.js
+++ src/pages/Request.js
@@ -1,19 +1,77 @@
 import React from "react";
-function Request() {
+
+export default function Request(props) {
+  const [fullname, setFullName] = React.useState("");
+  const [haweye, setHaweye] = React.useState("");
+  const [sub, setSub] = React.useState("");
+  const [content, setContent] = React.useState("");
+
+  function Click(e) {
+    e.preventDefault();
+    const data = { fullname, haweye, sub, content };
+    console.log(data);
+    fetch("http://localhost:4000/request", {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: { "content-type": "application/json" },
+    })
+      .then((res) => {
+        if (res.status === 201) {
+          //   <Route path="/Profile" component={lastPage} /> need a new page
+          console.log("success!");
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
   return (
     <div>
-      <h2>Name :</h2>
-      <input></input>
-      <h2>ID :</h2>
-      <input></input>
-      <h2>Subject Of Request :</h2>
-      <input></input>
-      <h2>Your Request :</h2>
-      <textarea className="text"></textarea>
-      <br></br>
-      <button className="btn">Submit</button>
+      <h6>date of today</h6>
+      <h3>Contact us:</h3>
+      <div>
+        <label>Full Name:</label>
+        <input
+          id="fullname"
+          name="fullname"
+          value={fullname}
+          onChange={(event) => setFullName(event.target.value)}
+          required
+        ></input>
+      </div>
+      <div>
+        <label>ID No. :</label>
+        <input
+          id="haweye"
+          name="haweye"
+          value={haweye}
+          onChange={(event) => setHaweye(event.target.value)}
+          required
+        ></input>
+      </div>
+      <div>
+        <label>Subject of request:</label>
+        <input
+          id="sub"
+          name="sub"
+          value={sub}
+          onChange={(event) => setSub(event.target.value)}
+          required
+        ></input>
+      </div>
+      <div>
+        <label>Your request:</label>
+        <input
+          id="content"
+          name="content"
+          value={content}
+          onChange={(event) => setContent(event.target.value)}
+          required
+        ></input>
+      </div>
+      <button className="btn" onClick={Click}>
+        Send
+      </button>
     </div>
   );
 }
-
-export default Request;
--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -10,61 +10,105 @@ export default function SignUp(props) {
   const [pass2, setPass2] = React.useState("");
 
   function Click(e) {
-      e.preventDefault();
-      const data = { fullname, haweye, phone, email, address, pass1, pass2 };
-      console.log(data);
-      fetch("http://localhost:4000/signUp", {
-          method: "POST",
-          body: JSON.stringify(data),
-          headers: { "content-type": "application/json" },
-      })
+    e.preventDefault();
+    const data = { fullname, haweye, phone, email, address, pass1 };
+    console.log(data);
+    fetch("http://localhost:4000/signUp", {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: { "content-type": "application/json" },
+    })
       .then((res) => {
-          if ( res.status === 201){
-              // window.location.href = "/home"
-              console.log("success!");
-      }})
-      .catch((error) => {
-          console.log(error);
+        if (res.status === 201) {
+          // window.location.href = "/home"
+          console.log("success!");
+        }
       })
+      .catch((error) => {
+        console.log(error);
+      });
   }
   return (
     <div>
       <h6>Please enter your details</h6>
       <div>
         <label>Full Name:</label>
-        <input id="fullname" name="fullname" value={fullname} onChange={(event) => setFullName(event.target.value)} required></input>
+        <input
+          id="fullname"
+          name="fullname"
+          value={fullname}
+          onChange={(event) => setFullName(event.target.value)}
+          required
+        ></input>
       </div>
       <div>
         <label>ID No. :</label>
-        <input id="haweye" name="haweye" value={haweye} onChange={(event) => setHaweye(event.target.value)} required></input>
+        <input
+          id="haweye"
+          name="haweye"
+          value={haweye}
+          onChange={(event) => setHaweye(event.target.value)}
+          required
+        ></input>
       </div>
       <div>
         <label>Phone No. :</label>
-        <input id="phone" name="phone" value={phone} onChange={(event) => setPhone(event.target.value)} required></input>
+        <input
+          id="phone"
+          name="phone"
+          value={phone}
+          onChange={(event) => setPhone(event.target.value)}
+          required
+        ></input>
       </div>
       <div>
         <label>Email:</label>
-        <input id="email" name="email" value={email} onChange={(event) => setEmail(event.target.value)} required></input>
+        <input
+          id="email"
+          name="email"
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
+          required
+        ></input>
       </div>
       <div>
         <label>Address:</label>
-        <input id="address" name="address" value={address} onChange={(event) => setAddress(event.target.value)} required></input>
+        <input
+          id="address"
+          name="address"
+          value={address}
+          onChange={(event) => setAddress(event.target.value)}
+          required
+        ></input>
       </div>
       <h6>Create a password</h6>
       <div>
         <label>Password:</label>
-        <input id="pass1" name="pass1" value={pass1} onChange={(event) => setPass1(event.target.value)} required></input>
+        <input
+          id="pass1"
+          name="pass1"
+          type="password"
+          value={pass1}
+          onChange={(event) => setPass1(event.target.value)}
+          required
+        ></input>
       </div>
       <div>
         <label>Password Again:</label>
-        <input id="pass2" name="pass2" value={pass2} onChange={(event) => setPass2(event.target.value)} required></input>
+        <input
+          id="pass2"
+          name="pass2"
+          type="password"
+          value={pass2}
+          onChange={(event) => setPass2(event.target.value)}
+          required
+        ></input>
       </div>
       <div>
-        <button className="btn" onClick={Click}>Sign Up</button>
+        <button className="btn" onClick={Click}>
+          Sign Up
+        </button>
       </div>
     </div>
-
   );
 }
-
-
